34617bee28ac649dce3a13dda9ac93bb9bad751f
Jesse Garcia <68394599+jgdovis@users.noreply.github.com>
2020-11-11 22:13:13 -0600

    Add files via upload

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer, network, accounts) {
+
+    deployer.deploy(FlightSuretyData, {gas: 6500000})
+    .then(() => FlightSuretyData.deployed())
+    .then((dataInstance) => {
+        return deployer.deploy(FlightSuretyApp, dataInstance.address, {gas: 6500000})
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:7545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
+                        }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+    });
+}
